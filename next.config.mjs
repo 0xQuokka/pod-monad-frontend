46e3e8511ae4021f4a53e77880fba1ea49fa4288
qq <sr@modular.pk>
2024-04-07 23:49:55 -0300

    add next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,56 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return {
+			beforeFiles: [
+				{
+					source: "/_next/:path*",
+					has: [
+						{
+							type: "host",
+							value: "app.pod.finance",
+						},
+					],
+					destination: "https://pod.finance/_next/:path*",
+				},
+				{
+					source: "/pod_favicon.png",
+					has: [
+						{
+							type: "host",
+							value: "app.pod.finance",
+						},
+					],
+					destination: "https://pod.finance/pod_favicon.png",
+				},
+				{
+					source: "/:path*",
+					has: [
+						{
+							type: "host",
+							value: "app.pod.finance",
+						},
+					],
+					destination: "/app/:path*",
+				},
+			],
+			fallback: [
+				{
+					source: "/_next/:path*",
+					has: [
+						{
+							type: "host",
+							value: "app.pod.finance",
+						},
+					],
+					destination: "https://pod.finance/_next/:path*",
+				},
+			],
+		};
+	},
+	images: {
+		domains: ["pod.finance", "app.pod.finance"],
+	},
+};
 
-export default nextConfig;
+module.exports = nextConfig;
