3d47a829d145f7bb3dc88581218d957216bef32c
qq <sr@modular.pk>
2024-04-26 20:40:55 -0300

    add genesis pod pod

--- config/genesis.ts
+++ config/genesis.ts
@@ -1,4 +1,3 @@
 export const GENESIS_PODS: Record<string, boolean> = {
-	"0xf413251b3fd9d955b656a5f10248509d94e4cf4c": true,
-	"0xf71a8aa99edd15b50bac74e6b4bbfcb0bdeb993f": true,
+	"0x7ebab0a15a11136bfe3abfa3663e4ba150607c02": true,
 };
