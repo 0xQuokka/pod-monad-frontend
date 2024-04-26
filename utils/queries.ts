c207237d144244856cbbc274a9453d4ff21e3851
qq <sr@modular.pk>
2024-04-26 03:30:17 -0300

    add queries utils

---
+++ utils/queries.ts
@@ -0,0 +1,41 @@
+import { gql } from "@apollo/client";
+
+export const getPodsQuery = (underlying: false | string = false) => {
+	return gql`
+		query GetPods {
+			podFactories(first: 1) {
+				availablePods
+			}
+			pods(first: 20, orderBy: reserve, orderDirection: desc, ${underlying ? `where: { underlying: "${underlying.toLowerCase()}" }` : ""}) {
+				id
+				name
+				symbol
+				reserve
+				locked
+				decimals
+				description
+				underlying {
+					id
+					name
+					symbol
+					decimals
+				}
+				decimals
+				rewards {
+					id
+					amount
+					remainingAmount
+					token {
+						id
+						name
+						symbol
+						decimals
+					}
+				}
+				owner {
+					id
+				}
+			}
+		}
+	`;
+};
