15a2c6bdeb30f9b3603898e06ac0cd4fc2264738
qq <sr@modular.pk>
2024-05-02 18:13:47 -0300

    add warpie lp

--- config/genesis.ts
+++ config/genesis.ts
@@ -2,6 +2,7 @@ export const GENESIS_PODS: Record<string, boolean> = {
 	"0x7ebab0a15a11136bfe3abfa3663e4ba150607c02": true,
 	"0xca4aed99a2c62cab5b4b6269c02fdb25dc4bd987": true,
 	"0x394212bacfb3e25ad6ca19abe8189a4810296a1f": true,
+	"0xfb8e5ff79e839b7351f85ee32c772885a7c1872e": true,
 };
 
 export const OVERRIDE_POD_DATA: Record<string, Record<string, string>> = {
